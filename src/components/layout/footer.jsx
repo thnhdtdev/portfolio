6432b910e436ce1052547be3cefd302a7e271ec9
thnhdtdev <thnhdat.dev@gmail.com>
2025-08-22 16:01:32 +0700

    ♻️  refacter: UI footer

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -1,27 +1,51 @@
 import { Github, Linkedin, Mail } from "lucide-react";
 
+const socials = [
+	{
+		icon: <Github />,
+		url: "https://github.com/thnhdtdev",
+		label: "Github"
+	},
+	{
+		icon: <Linkedin />,
+		url: "https://www.linkedin.com/in/v%C3%B5-th%C3%A0nh-%C4%91%E1%BA%A1t-b5251a2b7/",
+		label: "Linkedin"
+	},
+	{
+		icon: <Mail />,
+		url: "mailto:vodat0301@gmail.com",
+		label: "Mail"
+	}
+];
+
 const Footer = () => {
 	return (
-		<div className="px-80 h-44 bg-[#0b1523]">
-			<div className="flex justify-start items-center h-full">
-				<div>
+		<div className="px-4 md:px-20 lg:px-80 py-20 h-44 bg-[#0b1523]">
+			<div className="flex flex-col md:flex-row justify-between items-center h-full gap-8">
+				<div className="text-center md:text-left">
 					<h1 className="font-bold text-2xl text-blue-500 tracking-wide select-none uppercase">
 						thnhdtdev
 					</h1>
-					<p>
-						Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-						Lorem Ipsum has been the industry's standard dummy text ever since the
-						1500s, when an unknown printer took a galley of type and scrambled it to
-						make a type specimen book.
+					<p className="max-w-md text-sm text-gray-300">
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry...
 					</p>
 				</div>
-				<div>
-					<div className="flex items-center gap-4">
-						<Github />
-						<Linkedin />
-						<Mail />
+
+				<div className="flex flex-col gap-4 md:items-end">
+					<div className="flex items-center gap-6 justify-center">
+						{socials.map((item) => (
+							<a
+								key={item.label}
+								href={item.url}
+								className="hover:text-blue-500 transition-colors"
+							>
+								{item.icon}
+							</a>
+						))}
 					</div>
-					<div>© 2025 thnhdtdev | Built with React and Tailwind</div>
+					<div className="text-xs text-gray-400 text-center md:text-right">
+						© 2025 thnhdtdev | Built with React & Tailwind
+					</div>
 				</div>
 			</div>
 		</div>
--- src/view/homepage.jsx
+++ src/view/homepage.jsx
@@ -1,4 +1,4 @@
 const HomePage = () => {
-	return <div className="px-80">home page</div>;
+	return <div className="px-80 h-[1000px]">home page</div>;
 };
 export default HomePage;
