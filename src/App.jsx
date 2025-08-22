9780a34b27d3f6d191ea5e68ad67c71f8ebf0a59
thnhdtdev <thnhdat.dev@gmail.com>
2025-08-22 13:14:29 +0700

    🗑️  chore: setup posstcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,21 @@
-import "@/App.css";
-import {Route, Routes} from "react-router";
+import { Routes, Route } from "react-router-dom";
 
 import Header from "@/component/layout/header.jsx";
 import Footer from "@/component/layout/footer.jsx";
 import HomePage from "@/view/homepage.jsx";
 
 function App() {
-	return (
-        <Routes>
-            <Route
-                path="*"
-                element={
-                    <div className="h-screen flex flex-col">
-                        <Header />
-                        <main className="flex-1">
-                            <Routes>
-                                <Route path="/" element={<HomePage />} />
-                            </Routes>
-                        </main>
-                        <Footer />
-                    </div>
-                }
-            />
-        </Routes>
-
-
-
-	);
+    return (
+        <div className="h-screen flex flex-col">
+            <Header />
+            <main className="flex-1">
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                </Routes>
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- src/component/layout/header.jsx
+++ src/component/layout/header.jsx
@@ -1,8 +1,8 @@
 const Header = () =>{
     return(
-        <div>
-            header
+        <div className="bg-blue-500">
+            <h1 className="text-white text-3xl font-bold text-center py-4">Welcome to My Website</h1>
         </div>
     )
 }
-export default Header
+export default Header;
