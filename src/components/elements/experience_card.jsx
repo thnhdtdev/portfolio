2a7390c0090d3162773e9fac40323408648d76f9
thnhdtdev <thnhdat.dev@gmail.com>
2025-09-06 17:21:55 +0700

    ✨  feat:component experience card

--- src/components/elements/experience_card.jsx
+++ src/components/elements/experience_card.jsx
@@ -1,12 +1,11 @@
 const ExperienceCard = () => {
 	return (
 		<div className="flex flex-col border-2 border-primary rounded-md p-4">
-			<div className="text-lg font-semibold mb-3">Education</div>
-			<div className="flex flex-col gap-4 text-sm text-muted-foreground">
-				<div className="font-medium text-lg text-white">BSc in Information Systems</div>
-				<div>Ho Chi Minh City University of Industry and Trade</div>
-				<div>2020 – 2024</div>
-				<div>Graduated</div>
+			<div className="text-lg font-semibold mb-3">Experience</div>
+			<div className="flex flex-col gap-4 text-sm ">
+				<div className="font-medium text-lg">FrontEnd Developer</div>
+				<div className="text-muted-foreground">BachGiaIT</div>
+				<div className="text-muted-foreground">Nov 2024 – Sep 2025</div>
 			</div>
 		</div>
 	);
