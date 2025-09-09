12908815cef4acacb3ee3afa330cf22ea048b575
thnhdtdev <thnhdat.dev@gmail.com>
2025-09-09 15:19:49 +0700

    ✨  feat: add button show more projects

--- src/components/elements/project_card.jsx
+++ src/components/elements/project_card.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import { FaGithub } from "react-icons/fa6";
-
 import { Badge } from "@/components/ui/badge.jsx";
 import { Button } from "@/components/ui/button.jsx";
+import { IoCode } from "react-icons/io5";
 
 const ProjectCard = () => {
+	const [showMore, setShowMore] = useState(false);
+
 	const projects = [
 		{
 			image: "https://picsum.photos/128/128",
@@ -29,37 +32,55 @@ const ProjectCard = () => {
 	];
 
 	return (
-		<div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 ">
-			{projects.map((item) => {
-				return (
-					<div className="border mx-2 border-primary rounded-md hover:-translate-y-1 transition cursor-pointer duration-500">
-						<div>
-							<img
-								src={item?.image}
-								alt=""
-								className="h-48 w-96 object-cover rounded-t-md "
-							/>
-							<div className="flex flex-col flex-grow p-6 gap-6 justify-center items-start">
-								<div className="font-bold line-clamp-2 min-h-10">{item?.title}</div>
-								<div className="text-muted-foreground text-sm line-clamp-3">
-									{item?.description}
+		<div className="flex flex-col justify-center items-center gap-8">
+			<div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 ">
+				{projects.map((item) => {
+					return (
+						<div className="border mx-2 border-primary rounded-md hover:-translate-y-1 transition cursor-pointer duration-500">
+							<div>
+								<img
+									src={item?.image}
+									alt=""
+									className="h-48 w-96 object-cover rounded-t-md "
+								/>
+								<div className="flex flex-col flex-grow p-6 gap-6 justify-center items-start">
+									<div className="font-bold line-clamp-2 min-h-10">
+										{item?.title}
+									</div>
+									<div className="text-muted-foreground text-sm line-clamp-3">
+										{item?.description}
+									</div>
+									<div className="flex gap-1">
+										{item?.badge?.map((badge, idx) => (
+											<Badge variant="default" key={idx}>
+												{badge}
+											</Badge>
+										))}
+									</div>
+									<Button
+										variant="ghost"
+										size="sm"
+										className="border border-primary"
+									>
+										<FaGithub />
+										Github
+									</Button>
 								</div>
-								<div className="flex gap-1">
-									{item?.badge?.map((badge, idx) => (
-										<Badge variant="default" key={idx}>
-											{badge}
-										</Badge>
-									))}
-								</div>
-								<Button variant="ghost" size="sm" className="border border-primary">
-									<FaGithub />
-									Github
-								</Button>
 							</div>
 						</div>
-					</div>
-				);
-			})}
+					);
+				})}
+			</div>
+
+			<Button
+				variant="ghost"
+				size="lg"
+				className="border border-white rounded-3xl text-white capitalize "
+				onClick={() => setShowMore(!showMore)}
+			>
+				{showMore ? "Show Less" : "Show More Projects"}
+				<IoCode />
+			</Button>
 		</div>
 	);
 };
